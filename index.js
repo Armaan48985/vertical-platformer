2330a1212a2b1707a4b82f52d55bc3311720ce8d
Armaan48985 <armaanthakur2308@gmail.com>
2024-03-27 23:14:09 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,172 @@
+﻿const canvas = document.querySelector("canvas");
+const c = canvas.getContext("2d");
+
+canvas.width = 1024;
+canvas.height = 576;
+const gravity = 0.5;
+
+const scaledCanvas = {
+  width: canvas.width / 4,
+  height: canvas.height / 4,
+};
+
+const floorCollisions2D = [];
+for (let i = 0; i < floorCollisions.length; i += 36) {
+  floorCollisions2D.push(floorCollisions.slice(i, i + 36));
+}
+
+const collisionBlocks = [];
+floorCollisions2D.forEach((row, y) => {
+  row.forEach((symbol, x) => {
+    if (symbol === 202) {
+      collisionBlocks.push(
+        new CollisionBlock({
+          position: {
+            x: x * 16,
+            y: y * 16,
+          },
+        })
+      );
+    }
+  });
+});
+
+const platformCollisions2D = [];
+for (let i = 0; i < platformCollisions.length; i += 36) {
+  platformCollisions2D.push(platformCollisions.slice(i, i + 36));
+}
+
+const platformCollisionBlocks = [];
+platformCollisions2D.forEach((row, y) => {
+  row.forEach((symbol, x) => {
+    if (symbol === 202) {
+      platformCollisionBlocks.push(
+        new CollisionBlock({
+          position: {
+            x: x * 16,
+            y: y * 16,
+          },
+        })
+      );
+    }
+  });
+});
+
+const keys = {
+  d: {
+    keypressed: false,
+  },
+  a: {
+    keypressed: false,
+  },
+};
+
+class Sprite {
+  constructor({ position, imageSrc }) {
+    this.position = position;
+    this.image = new Image();
+    this.image.src = imageSrc;
+  }
+
+  draw() {
+    if (!this.image) return;
+    c.drawImage(this.image, this.position.x, this.position.y);
+  }
+
+  update() {
+    this.draw();
+  }
+}
+
+const background = new Sprite({
+  position: {
+    x: 0,
+    y: 0,
+  },
+  imageSrc: "./background.png",
+});
+
+class Player {
+  constructor(position) {
+    this.position = position;
+    this.velocity = {
+      x: 0,
+      y: 1,
+    };
+    this.height = 100;
+  }
+
+  draw() {
+    c.fillStyle = "red";
+    c.fillRect(this.position.x, this.position.y, 100, this.height);
+  }
+
+  update() {
+    player.draw();
+    this.position.y += this.velocity.y;
+    this.position.x += this.velocity.x;
+    if (this.position.y + this.height + this.velocity.y < canvas.height) {
+      this.velocity.y += gravity;
+    } else {
+      this.velocity.y = 0;
+    }
+  }
+}
+
+const player = new Player({
+  x: 0,
+  y: 0,
+});
+
+let y = 100;
+
+function animate() {
+  window.requestAnimationFrame(animate);
+  c.fillStyle = "white";
+  c.fillRect(0, 0, canvas.width, canvas.height);
+
+  c.save();
+  c.scale(4, 4);
+  c.translate(0, -background.image.height + scaledCanvas.height);
+  background.update();
+  collisionBlocks.forEach((collisionBlock) => {
+    collisionBlock.update();
+  });
+  platformCollisionBlocks.forEach((collisionBlock) => {
+    collisionBlock.update();``
+  });
+  c.restore();
+
+  player.update();
+
+  player.velocity.x = 0;
+  if (keys.d.keypressed) player.velocity.x = 1;
+  else if (keys.a.keypressed) player.velocity.x = -1;
+}
+
+animate();
+
+window.addEventListener("keydown", (event) => {
+  switch (event.key) {
+    case "d":
+      keys.d.keypressed = true;
+      break;
+    case "a":
+      keys.a.keypressed = true;
+      break;
+    case "w":
+      player.velocity.y = -20;
+      break;
+  }
+});
+
+window.addEventListener("keyup", (event) => {
+  switch (event.key) {
+    case "d":
+      keys.d.keypressed = false;
+      break;
+    case "a":
+      keys.a.keypressed = false;
+      break;
+  }
+});
